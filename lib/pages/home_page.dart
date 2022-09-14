e500db36725eef91117c4aef9f0201a6a239e4e6
Soikat77 <soikatalam26@gmail.com>
2022-09-14 23:35:48 +0600

    splash screen aka sign in screen added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isAuth = false;
+
+  Widget buildAuthScreen() {
+    return const Text('Authenticated');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.teal,
+              Colors.purple,
+            ],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Instagram',
+              style: TextStyle(
+                fontFamily: "Back To School",
+                fontSize: 64,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 18.0),
+            GestureDetector(
+              onTap: () => print('tapped'),
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/sign_in.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
