139b9f9a0a8d128cde8ecd0fba991a7ed8134a58
Soikat77 <soikatalam26@gmail.com>
2022-09-15 22:00:10 +0600

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +12,29 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool isAuth = false;
+
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      if (account != null) {
+        print('User signed in!: $account');
+        setState(
+          () {
+            isAuth = true;
+          },
+        );
+      } else {
+        setState(() {
+          isAuth = false;
+        });
+      }
+    });
+  }
+
+  logIn() {
+    googleSignIn.signIn();
+  }
 
   Widget buildAuthScreen() {
     return const Text('Authenticated');
@@ -42,7 +68,7 @@ class _HomePageState extends State<HomePage> {
             ),
             const SizedBox(height: 18.0),
             GestureDetector(
-              onTap: () => print('tapped'),
+              onTap: logIn,
               child: Container(
                 width: 260.0,
                 height: 60.0,
