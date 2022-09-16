789f6d5091f664f58eb032007e5194f725b19c99
Soikat77 <soikatalam26@gmail.com>
2022-09-16 20:44:31 +0600

    create, update, delete method added

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -3,6 +3,7 @@ import 'package:instagram/widgets/header.dart';
 import 'package:instagram/widgets/progress.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// catching the database collection
 final CollectionReference userRef =
     FirebaseFirestore.instance.collection('users');
 
@@ -14,6 +15,43 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    // createUser();
+    // updateUser();
+    // deleteUser();
+    super.initState();
+  }
+
+// creating a new user
+  createUser() {
+    userRef.doc("abcdefg").set({
+      "username": "Jeff",
+      "postCount": 0,
+      "isAdmin": false,
+    });
+  }
+
+  // updating the user
+  updateUser() async {
+    final doc = await userRef.doc("abcdefg").get();
+    if (doc.exists) {
+      doc.reference.update({
+        "username": "Jeff Bejos",
+        "postCount": 0,
+        "isAdmin": false,
+      });
+    }
+  }
+
+  // deliting a user
+  deleteUser() async {
+    final doc = await userRef.doc("abcdefg").get();
+    if (doc.exists) {
+      doc.reference.delete();
+    }
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
